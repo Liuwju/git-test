2f7df2d95f8797aeea659902c20e6559cf807d6e
lwjun <lwjun@qq.com>
2022-07-16 16:34:23 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @author lwJun
+ * @create 2022-07-16 16:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
